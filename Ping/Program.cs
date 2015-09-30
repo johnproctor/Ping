881c677160ddedf67aa0b6d19a1c61fbfd7c4c74
John Proctor <jceproctor@googlemail.com>
2015-09-30 19:53:45 +0100

    Commit

---
+++ Ping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Ping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = ConfigurationManager.AppSettings["Url"];
+
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var result = client.DownloadString(url);
+                    Console.WriteLine(String.Format("Called: {0} Result: {1}", url, result));
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(String.Format("Error calling: {0}", url));
+            }
+
+
+        }
+    }
+}
